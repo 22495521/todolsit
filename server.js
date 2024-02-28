03d423e1de757d1754616d030498f27b27026d56
BigTO <d22495521@gmail.com>
2024-02-28 10:57:08 +0800

    更新hostman

--- server.js
+++ server.js
@@ -86,8 +86,5 @@ const server = http.createServer((req, res) => {
   }
 });
 
-const hostname = process.env.PORT || "127.0.0.1";
-const port = 3000;
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+const port = process.env.PORT || 3000;
+server.listen(port);
