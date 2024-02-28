d80867ffefea562b9443db4b05e56c6b5e3f3b10
BigTO <d22495521@gmail.com>
2024-02-28 10:48:14 +0800

    更新padckage.json

--- server.js
+++ server.js
@@ -86,7 +86,7 @@ const server = http.createServer((req, res) => {
   }
 });
 
-const hostname = "127.0.0.1";
+const hostname = process.env.PORT || "127.0.0.1";
 const port = 3000;
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
